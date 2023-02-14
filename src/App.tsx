4f3a70979b42114cd50341c5c5ad4c46e7aa8095
RodrigoStrapazzon <Rodrigo93rs@gmail.com>
2023-02-14 17:41:37 -0300

    CyclesContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
-import { GlobalStyle } from './Styles/global'
-import { defaultTheme } from './Styles/themes/default'
+import { GlobalStyle } from "./Styles/global";
+import { defaultTheme } from "./Styles/themes/default";
+import { CyclesContextProvider } from "./contexts/CyclesContext";
+
+
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
